149bc84e2cee1de992c37c85e23138c9ed988202
Elther <eltherb@magenic.com>
2020-04-22 20:12:08 +0800

    test changes for deployment

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -16,7 +16,7 @@ class SearchBar extends React.Component {
             <div className="search-bar ui segment">
                 <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
-                        <label>Video Search</label>
+                        <label>Video Search (by elther)</label>
                         <input
                             type="text"
                             value={this.state.term}
