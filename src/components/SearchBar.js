3e9c7f9a97d3173df0e29e783607dbf646bb9547
Elther <eltherb@magenic.com>
2020-04-22 21:13:23 +0800

    test for build and deploy aws

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -16,7 +16,7 @@ class SearchBar extends React.Component {
             <div className="search-bar ui segment">
                 <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
-                        <label>Video Search (by elther)</label>
+                        <label>Video Search (--edited for aws build test)</label>
                         <input
                             type="text"
                             value={this.state.term}
